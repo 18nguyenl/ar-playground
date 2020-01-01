f9389c83ecfe32c586c3307905e8ef9a55b4fc7d
Long Nguyen <18nguyenl@gmail.com>
2019-12-31 18:47:31 -0600

    Add touch placement

---
+++ Assets/Scripts/PlaceObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceObject : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Object;
+
+    private ARRaycastManager RaycastManager;
+    
+    // Start is called before the first frame update
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (RaycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = hits[0].pose;
+
+                    Instantiate(m_Object, hitPose.position, hitPose.rotation);
+                }
+            }
+        }
+    }
+}
